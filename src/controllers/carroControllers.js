132bee6d084714da415f6011a79791b73097a385
Luis Ricardo <luisricardofs@gmail.com>
2024-04-06 18:36:04 -0300

    Criando GET nas classes Controller,routes,service

--- src/controllers/carroControllers.js
+++ src/controllers/carroControllers.js
@@ -1,1 +1,29 @@
 const carroServices = require('../services/carroServices');
+
+module.exports = {
+    buscarTodos: async (req, res) => {
+        let json = {error: '', result:[]};
+
+        let carros = await carroServices.buscarTodos();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                descricao: carros[i].modelo
+            });
+        }
+        res.json(json);
+    },
+
+    buscarUm: async (req, res) => {
+        let json = {error: '', result:{}};
+
+        let codigo = req.params.codigo;
+        let carro = await carroServices.buscarUm(codigo);
+
+        if(carro){
+            json.result = carro;
+        }
+        res.json(json);
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const carroController = require('./controllers/carroControllers');
 
+router.get('/carros', carroController.buscarTodos);
+router.get('/carros/:codigo', carroController.buscarUm);
 module.exports = router;
