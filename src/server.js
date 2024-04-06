fc66c4ff23fb87268510f6d473b2ca2cbbdd7d4a
Luis Ricardo <luisricardofs@gmail.com>
2024-04-06 11:37:58 -0300

    Configurando a aplicação

---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config({path:'variaveis.env'});
+const express = require('express')
+const cors = require('cors');
+const bodyparser = require('body-parser');//converte o 
+
+const routers = require('./routes');//informa onde estão as rotas do projeto
+const bodyParser = require('body-parser');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.listen(process.env.PORT, () =>{
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+})
